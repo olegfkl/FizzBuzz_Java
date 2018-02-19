b7d2ab29daaebe3c5a7c0393731b1e925be9eed4
Oleg Grushetskyy <olegfkl@gmail.com>
2018-02-19 16:31:50 +0000

    Pass all unit tests

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,5 +1,13 @@
 public class FizzBuzz {
     public String play(int a) {
-        return "Fizz";
+        if (a % 3 == 0 && a % 5 == 0) {
+            return "FizzBuzz";
+        } else if (a % 3 == 0) {
+            return "Fizz";
+        } else if (a % 5 == 0){
+            return "Buzz";
+        } else {
+            return String.valueOf(a);
+        }
     }
 }
--- test/FizzBuzzTest.java
+++ test/FizzBuzzTest.java
@@ -5,11 +5,30 @@ import static org.junit.Assert.*;
 public class FizzBuzzTest {
 
     @Test
-
-    public void test() {
+    public void testFizz() {
         FizzBuzz test  = new FizzBuzz();
         String result = test.play(3);
         assertEquals("Fizz", result);
     }
+
+    @Test
+    public void testBuzz() {
+        FizzBuzz anotherInstance  = new FizzBuzz();
+        assertEquals("Buzz", anotherInstance.play(5));
+    }
+
+    @Test
+    public void testFizzBuzz() {
+        FizzBuzz yetAnotherInstance  = new FizzBuzz();
+        assertEquals("FizzBuzz", yetAnotherInstance.play(15));
+    }
+
+    @Test
+    public void testDefault() {
+        FizzBuzz defaultNumber = new FizzBuzz();
+        assertEquals("17", defaultNumber.play(17));
+    }
+
+
 
 }
